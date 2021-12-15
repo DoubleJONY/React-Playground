75adbbe2f00f402e2ff45be03b72ab0e3ee0901d
JONY <jony@mail.i-on.net>
2021-12-15 11:01:10 +0900

    1.5.4 props.children

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Hello from './Hello';
+import Wrapper from './Wrapper';
 
 function App() {
     return (
-        <>
+        <Wrapper>
             <Hello name="react" color="red"/>
             <Hello color="pink"/>
             <Hello name="jony" color="blue"/>
             <Hello color="cyan"/>
-        </>
+        </Wrapper>
     );
 }
 
