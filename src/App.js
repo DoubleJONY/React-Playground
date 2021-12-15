2006211f959ae4b1476c51978b36911fcdefd8ce
JONY <jony@mail.i-on.net>
2021-12-15 11:05:40 +0900

    1.6.1 Conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 
+
 function App() {
     return (
         <Wrapper>
-            <Hello name="react" color="red"/>
-            <Hello color="pink"/>
-            <Hello name="jony" color="blue"/>
-            <Hello color="cyan"/>
+            <Hello name="react" color="red" isSpecial={true}/>
+            <Hello color="pink" />
         </Wrapper>
-    );
+    )
 }
 
 export default App;
