543f1b0cc0e7fe2f1f9e8cc655d6e02ac9f3861a
JONY <jony@mail.i-on.net>
2022-01-03 11:02:30 +0900

    1.17.1 useMemo 클릭한 사용자 수를 count 그런데 input만 변경되어도 계속 렌더됨

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import React, { useRef, useState } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
-const App = () => {
-    const [input, setInput] = useState({
+function countActiveUsers(users) {
+    console.log('활성 사용자 수를 세는중...');
+    return users.filter(user => user.active).length;
+}
+
+function App() {
+    const [inputs, setInputs] = useState({
         username: '',
-        email: '',
-        id: ''
-    })
-
-    const {username, email, id} = input;
-
-    const onChange =(e)=>{
-        const {name, value} = e.target;
-        setInput({
-            ...input,
-            [name]:value
-        })
-    }
-
+        email: ''
+    });
+    const { username, email } = inputs;
+    const onChange = e => {
+        const { name, value } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    };
     const [users, setUsers] = useState([
         {
             id: 1,
@@ -41,58 +42,46 @@ const App = () => {
     ]);
 
     const nextId = useRef(4);
-
     const onCreate = () => {
-        const newUser ={
+        const user = {
             id: nextId.current,
-            username:username,
-            email:email
-        }
-        setUsers([
-            ...users, newUser
-        ])
-        setInput({
+            username,
+            email
+        };
+        setUsers(users.concat(user));
+
+        setInputs({
             username: '',
             email: ''
-        })
-
+        });
         nextId.current += 1;
-    }
-    const onUpdate = () => {
-        setUsers(
-            users.map(user => user.id === id ? {...user, username: username, email : email} : user)
-        )
-        setInput({
-            username: '',
-            email: '',
-            id: '',
-        })
-    }
+    };
 
-    const onRemove = (id) => {
+    const onRemove = id => {
+        // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+        // = user.id 가 id 인 것을 제거함
         setUsers(users.filter(user => user.id !== id));
-    }
-
-    const onToggle = (id) => {
+    };
+    const onToggle = id => {
         setUsers(
-            users.map(user => user.id === id? {...user, active : !user.active} : user)
-        )
-    }
-
-    const onModify = (user) => {
-        setInput({
-            username: user.username,
-            email: user.email,
-            id: user.id
-        })
-    }
-
-    return(
-        <div>
-            <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} onUpdate={onUpdate}/>
-            <UserList users={users} onRemove={onRemove} onToggle={onToggle} onModify={onModify}/>
-        </div>
-    )
+            users.map(user =>
+                user.id === id ? { ...user, active: !user.active } : user
+            )
+        );
+    };
+    const count = countActiveUsers(users);
+    return (
+        <>
+            <CreateUser
+                username={username}
+                email={email}
+                onChange={onChange}
+                onCreate={onCreate}
+            />
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+            <div>활성사용자 수 : {count}</div>
+        </>
+    );
 }
 
 export default App;
